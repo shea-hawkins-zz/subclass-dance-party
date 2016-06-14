725284bc693485afe7ad453faf67d0abad8111ce
Hack Reactor Students <accounts@hackreactor.com>
2016-06-14 11:35:27 -0700

    Implement GhostHunters.

--- src/GhostHunter.js
+++ src/GhostHunter.js
@@ -1,8 +1,10 @@
-class MurrayDancer extends ImageDancer {
+var hunterImages = ['assets/bill.png', 'assets/darthvader.png', 'assets/genie.png', 'assets/luigi.png'];
+class GhostHunter extends ImageDancer {
   constructor(top, left, timeBetweenSteps, width, height) {
     var width = Math.floor(Math.random() * 200 + 50);
     var height = width;
-    var src = `http://fillmurray.com/${width}/${height}`;
+    var src = hunterImages.shift();
+    hunterImages.push(src);
     super(top, left, timeBetweenSteps, src);
   }
   step() {
@@ -10,4 +12,4 @@ class MurrayDancer extends ImageDancer {
   }
 }
 
-window['MurrayDancer'] = MurrayDancer;
+window['GhostHunter'] = GhostHunter;

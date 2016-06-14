476ed65c58ba7d74e9fbc70c0e78e384aaf08efc
Hack Reactor Students <accounts@hackreactor.com>
2016-06-14 15:17:58 -0700

    Implement ghostbusting beams.

--- src/GhostHunter.js
+++ src/GhostHunter.js
@@ -1,8 +1,6 @@
 var hunterImages = ['assets/bill.png', 'assets/darthvader.png', 'assets/genie.png', 'assets/luigi.png'];
 class GhostHunter extends ImageDancer {
   constructor(top, left, timeBetweenSteps, width, height) {
-    var width = Math.floor(Math.random() * 200 + 50);
-    var height = width;
     var src = hunterImages.shift();
     hunterImages.push(src);
     super(top, left, timeBetweenSteps, src);
@@ -15,7 +13,15 @@ class GhostHunter extends ImageDancer {
   step(entities) {
     super.step();
     var closest = _(entities).chain().filter(e => e instanceof Ghost).reduce(findClosest.bind(this)).value();
-    closest !== null && this.setDestination(closest.left, closest.top);
+    if (closest) {
+      this.setDestination(closest.left, closest.top);
+      if (this.computeDistance(closest) < 50 && !this.beamOn) {
+        this.toggleBeam();
+        setTimeout(this.toggleBeam.bind(this), 100);
+        this.setBeamTarget(closest.left, closest.top);
+      }
+    }
+
     function findClosest(mem, entity) {
       return this.computeDistance(entity) < this.computeDistance(mem) ? entity : mem;
     }
@@ -29,12 +35,12 @@ class GhostHunter extends ImageDancer {
   render(ctx) {
     super.render(ctx);
     // render beam if on
-    if (this.beamTarget.length === 2) {
+    if (this.beamOn) {
       let [tx, ty] = this.beamTarget;
       ctx.beginPath();
       ctx.strokeStyle = '#f00';
-      ctx.moveTo(this.left, this.top);
-      ctx.lineTo(tx, ty);
+      ctx.moveTo(this.left + 50, this.top + 50);
+      ctx.lineTo(tx + 50, ty + 50);
       ctx.stroke();
     }
   }
